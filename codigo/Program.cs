beaecc3174a2180b9ebbe3ffe1f36c314b006ce9
YoshuJumpMaster <ian.matsuhara@gmail.com>
2024-04-28 14:45:47 -0300

    etapa3

--- codigo/Program.cs
+++ codigo/Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using LocadoraDeVeiculos.Models;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+using System.IO;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,7 +10,15 @@ builder.Services.AddDbContext<LocadoraContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "LocadoraDeVeiculos", Version = "v1" });
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFilename);
+    c.IncludeXmlComments(xmlPath);
+});
+
+
 
 var app = builder.Build();
 
@@ -19,13 +30,26 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-
+/// <summary>
+/// Obtém uma lista de todos os veículos.
+/// </summary>
+/// <returns>Uma lista de veículos.</returns>
+/// <response code="200">Se a lista de veículos for recuperada com sucesso.</response>
 app.MapGet("/veiculos", async (LocadoraContext context) =>
 {
     var veiculos = await context.Veiculos.ToListAsync();
     return Results.Ok(veiculos);
 });
 
+
+
+/// <summary>
+/// Obtém o véiculo com aquele ID.
+/// </summary>
+/// <param name="id">O ID do veículo a ser obtido.</param>
+/// <returns>Os detalhes do veículo.</returns>
+/// <response code="200">Retorna o veículo encontrado.</response>
+/// <response code="404">Se nenhum veículo for encontrado com aquele ID.</response>
 app.MapGet("/veiculos/{id}", async (LocadoraContext context, int id) =>
 {
     var veiculo = await context.Veiculos.FindAsync(id);
@@ -39,12 +63,31 @@ app.MapGet("/veiculos/{id}", async (LocadoraContext context, int id) =>
     }
 });
 
+
+/// <summary>
+/// Cria veículo.
+/// </summary>
+/// <param name="veiculo">O objeto veículo com este nome será criado.</param>
+/// <returns>O veículo criado.</returns>
+/// <response code="201"> veículo criado com sucesso.</response>
+
 app.MapPost("/veiculos", async (LocadoraContext context, Veiculo veiculo) =>
 {
     context.Veiculos.Add(veiculo);
     await context.SaveChangesAsync();
     return Results.Created($"/veiculos/{veiculo.VeiculoId}", veiculo);
 });
+
+
+
+/// <summary>
+/// Atualiza veículo.
+/// </summary>
+/// <param name="id">ID do veículo a ser atualizado.</param>
+/// <param name="veiculoInput">dados atualizados do veículo.</param>
+/// <returns>Nenhum conteúdo, confirmando que o veículo foi atualizado.</returns>
+/// <response code="204">O veículo foi atualizado com sucesso.</response>
+/// <response code="404">Nenhum veículo encontrado com este ID.</response>
 
 app.MapPut("/veiculos/{id}", async (LocadoraContext context, int id, Veiculo veiculoInput) =>
 {
@@ -62,6 +105,15 @@ app.MapPut("/veiculos/{id}", async (LocadoraContext context, int id, Veiculo vei
     return Results.NoContent();
 });
 
+
+
+/// <summary>
+/// Deleta veículo.
+/// </summary>
+/// <param name="id">O ID do veículo a ser deletado.</param>
+/// <returns>Nenhum conteúdo, confirmando que o veículo foi deletado.</returns>
+/// <response code="204"> veículo deletado com sucesso.</response>
+/// <response code="404">Nenhum veículo encontrado com esse ID.</response>
 app.MapDelete("/veiculos/{id}", async (LocadoraContext context, int id) =>
 {
     var veiculo = await context.Veiculos.FindAsync(id);
@@ -73,12 +125,28 @@ app.MapDelete("/veiculos/{id}", async (LocadoraContext context, int id) =>
     await context.SaveChangesAsync();
     return Results.NoContent();
 });
+
+
+
+/// <summary>
+/// Obtém todos os clientes armazenados.
+/// </summary>
+/// <returns>Uma lista de clientes.</returns>
+/// <response code="200">Retorna a lista de clientes.</response>
 app.MapGet("/clientes", async (LocadoraContext context) =>
 {
     var clientes = await context.Clientes.ToListAsync();
     return Results.Ok(clientes);
 });
 
+
+/// <summary>
+/// Obtém o cliente com aquele ID.
+/// </summary>
+/// <param name="id">O ID do cliente.</param>
+/// <returns>Os detalhes do cliente.</returns>
+/// <response code="200">Retorna o cliente encontrado.</response>
+/// <response code="404">Se nenhum cliente para aquele ID.</response>
 app.MapGet("/clientes/{id}", async (LocadoraContext context, int id) =>
 {
     var cliente = await context.Clientes.FindAsync(id);
@@ -92,6 +160,15 @@ app.MapGet("/clientes/{id}", async (LocadoraContext context, int id) =>
     }
 });
 
+
+
+
+/// <summary>
+/// Cria cliente.
+/// </summary>
+/// <param name="cliente">dados do novo cliente.</param>
+/// <returns> Detalhes do cliente.</returns>
+/// <response code="201"> cliente criado.</response>
 app.MapPost("/clientes", async (LocadoraContext context, Cliente cliente) =>
 {
     context.Clientes.Add(cliente);
@@ -99,6 +176,19 @@ app.MapPost("/clientes", async (LocadoraContext context, Cliente cliente) =>
     return Results.Created($"/clientes/{cliente.ClienteId}", cliente);
 });
 
+
+
+
+
+
+
+/// <summary>
+/// Atualiza um cliente .
+/// </summary>
+/// <param name="id"> ID do cliente a ser atualizado.</param>
+/// <param name="clienteInput"> dados atualizados do cliente </param>
+/// <response code="204"> cliente foi atualizado com sucesso </response>
+/// <response code="404"> nenhum cliente  encontrado para esse ID.</response>
 app.MapPut("/clientes/{id}", async (LocadoraContext context, int id, Cliente clienteInput) =>
 {
     var cliente = await context.Clientes.FindAsync(id);
@@ -113,6 +203,14 @@ app.MapPut("/clientes/{id}", async (LocadoraContext context, int id, Cliente cli
     return Results.NoContent();
 });
 
+
+
+/// <summary>
+/// Excluir cliente .
+/// </summary>
+/// <param name="id"> ID do cliente a ser excluído </param>
+/// <response code="204"> Cliente excluído com sucesso </response>
+/// <response code="404"> nenhum cliente corresponde esse ID</response>
 app.MapDelete("/clientes/{id}", async (LocadoraContext context, int id) =>
 {
     var cliente = await context.Clientes.FindAsync(id);
@@ -124,12 +222,30 @@ app.MapDelete("/clientes/{id}", async (LocadoraContext context, int id) =>
     await context.SaveChangesAsync();
     return Results.NoContent();
 });
+
+
+
+/// <summary>
+/// Obtém reservas armazenadas.
+/// </summary>
+/// <returns>Uma lista de reservas.</returns>
+/// <response code="200">Retorna a lista de reservas.</response>
 app.MapGet("/reservas", async (LocadoraContext context) =>
 {
     var reservas = await context.Reservas.ToListAsync();
     return Results.Ok(reservas);
 });
 
+
+
+
+/// <summary>
+/// Obtém a reserva com aquele ID.
+/// </summary>
+/// <param name="id">O ID daquela reserva.</param>
+/// <returns>Os detalhes da reserva.</returns>
+/// <response code="200">Retorna a reserva encontrada.</response>
+/// <response code="404">Se nenhuma reserva armazenada corresponde com aquele ID.</response>
 app.MapGet("/reservas/{id}", async (LocadoraContext context, int id) =>
 {
     var reserva = await context.Reservas.FindAsync(id);
@@ -143,6 +259,15 @@ app.MapGet("/reservas/{id}", async (LocadoraContext context, int id) =>
     }
 });
 
+
+
+
+/// <summary>
+/// Cria reserva.
+/// </summary>
+/// <param name="reserva"> dados da reserva </param>
+/// <returns> detalhes da reserva </returns>
+/// <response code="201"> Retorna reserva criada </response>
 app.MapPost("/reservas", async (LocadoraContext context, Reserva reserva) =>
 {
     context.Reservas.Add(reserva);
@@ -151,6 +276,14 @@ app.MapPost("/reservas", async (LocadoraContext context, Reserva reserva) =>
 });
 
 
+
+/// <summary>
+/// Atualiza reserva .
+/// </summary>
+/// <param name="id"> ID da reserva a ser atualizada </param>
+/// <param name="reservaInput"> dados atualizados da reserva </param>
+/// <response code="204"> Reserva foi atualizada com sucesso </response>
+/// <response code="404"> nenhuma reserva encontrada com esse ID </response>
 app.MapPut("/reservas/{id}", async (LocadoraContext context, int id, Reserva reservaInput) =>
 {
     var reserva = await context.Reservas.FindAsync(id);
@@ -168,6 +301,14 @@ app.MapPut("/reservas/{id}", async (LocadoraContext context, int id, Reserva res
     return Results.NoContent();
 });
 
+
+
+/// <summary>
+/// Excluir reserva .
+/// </summary>
+/// <param name="id"> ID  da reserva para exluir </param>
+/// <response code="204"> reserva excluída com sucesso.</response>
+/// <response code="404"> nenhuma reserva corresponde esse ID</response>
 app.MapDelete("/reservas/{id}", async (LocadoraContext context, int id) =>
 {
     var reserva = await context.Reservas.FindAsync(id);
